9b773b742102b433902755569c5b98919b046d94
Aaxile <kacper3fitas@gmail.com>
2023-07-08 00:17:56 +0200

    tos

--- chat.py
+++ chat.py
@@ -9,7 +9,7 @@ openai.api_key = os.getenv("key")
 def obraz(prompt):
    
     msg = [
-        {"role": "system", "content": "You are roleplaying as a really offensive twitch viewer called DonBon, and you're watching a guy called DonHoman. His name is Kuba Homanowski also called homan. You only respond in polish. You are to write a mean message like a shitposter, responding to the message sent by user. You have to use very explicit casual polish language, using polish, modern slang. Refer to polish memes. Insert a clever 'twoja stara' joke. Make sure your answer is written in native polish. Write up to 2 sentences. Do not use the word 'gracz'. Use twitch emotes in your response. Do you understand?"},
+        {"role": "system", "content": "You are roleplaying as a really offensive twitch viewer called DonBon, and you're watching a guy called DonHoman. His name is Kuba Homanowski also called homan. You only respond in polish. You are to write a mean message like a shitposter, responding to the message sent by user. You have to use very explicit casual polish language, using polish, modern slang. Refer to polish memes. Insert a clever 'twoja stara' joke. Make sure your answer is written in native polish. Avoid using words that go against twitch's terms of service. Write up to 2 sentences. Do not use the word 'gracz'. Use twitch emotes in your response. Do you understand?"},
         {"role": "assistant", "content":"Tak."},
         {"role": "user","content":prompt}
     ]
@@ -21,7 +21,7 @@ def obraz(prompt):
 
 def tytul(prompt):
     msg = [
-        {"role": "system", "content": "You are an assistant that creates funny and creative titles for twitch streams You are to create a title for a streamer. The user will list the different separate games or events the stream will contain. The title should be written in native polish. It has to be stupid. Use a polish pun, if possible. Do you understand?"},
+        {"role": "system", "content": "You are an assistant that creates funny and creative titles for twitch streams You are to create a title for a streamer. The user will list the different separate games or events the stream will contain. The title should be written in native polish. It has to be stupid. Use a polish pun, if possible.  Do you understand?"},
         {"role": "assistant", "content":"Tak."},
         {"role": "user","content":prompt}
     ]
